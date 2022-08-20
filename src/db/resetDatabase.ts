06667207828181f10179a1af391052e3abcae39a
Jay <hollowic@hotmail.com>
2022-08-19 23:09:05 -0700

    Added more engraving seeds

---
+++ src/db/resetDatabase.ts
@@ -0,0 +1,46 @@
+// load .env data into process.env
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// other dependencies
+import { readFileSync, readdirSync } from 'fs';
+const Client = require('pg-native');
+
+// PG connection setup
+const connectionString = process.env.DATABASE_URL ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const client = new Client();
+
+// Loads the schema files from db/schema
+const runSchemaFiles = function() {
+  console.log('-> Loading Schema Files ...');
+  const schemaFilenames = readdirSync('./db/schema');
+
+  for (const fn of schemaFilenames) {
+    const sql = readFileSync(`./db/schema/${fn}`, 'utf8');
+    console.log(`-> Running ${fn}`);
+    client.querySync(sql);
+  }
+};
+
+const runSeedFiles = function() {
+  console.log('-> Loading Seeds ...');
+  const schemaFilenames = readdirSync('./db/seeds');
+
+  for (const fn of schemaFilenames) {
+    const sql = readFileSync(`./db/seeds/${fn}`, 'utf8');
+    console.log(`-> Running ${fn}`);
+    client.querySync(sql);
+  }
+};
+
+try {
+  console.log(`-> Connecting to PG using ${connectionString} ...`);
+  client.connectSync(connectionString);
+  runSchemaFiles();
+  runSeedFiles();
+  client.end();
+} catch (err) {
+  console.error(`Failed due to error: ${err}`);
+  client.end();
+}
