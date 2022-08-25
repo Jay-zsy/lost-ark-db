b46bddb001d4ca13cac16010b76ba1fe50a15fb1
Jay <hollowic@hotmail.com>
2022-08-25 03:13:22 -0700

    removed some client config options

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,10 @@ import { Client } from 'pg';
 import { ENV } from '../environment';
 export const client = new Client({
   connectionString: process.env.DATABASE_URL || "",
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
+  // user: process.env.PGUSER,
+  // password: process.env.PGPASSWORD,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
 });
 
 client.connect().catch(e => console.log(`Error connecting to Postgres server while in ${ENV} environment:\n${e}`));
